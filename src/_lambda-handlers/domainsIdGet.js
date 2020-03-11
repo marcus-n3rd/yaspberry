573b47c133209f2609239ec9488553e4a57dfacd
Marcus Bernal <marcus.bernal@slalom.com>
2020-03-11 11:14:32 -0600

    Temporarily disable authentication for the domainGetId endpoint.

--- src/_lambda-handlers/domainsIdGet.js
+++ src/_lambda-handlers/domainsIdGet.js
@@ -2,7 +2,13 @@
 import { domainGet } from '../controllers/domain.controller';
 import withHttpMiddleware from '../middleware/withHttpMiddleware';
 
+const midParams = {
+  auth: {
+    enabled: false,
+  },
+};
+
 export const handler = withHttpMiddleware(async event => {
   const { pathParameters: { id } } = event;
   return await domainGet(id);
-});
+}, midParams);
