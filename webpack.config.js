665ff4ab683a30e05d4b64e020b723b116229360
Marcus Bernal <marcus.bernal@slalom.com>
2020-03-11 14:10:34 -0600

    Determine webpack mode based on the serverless stage.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  mode: slsw.lib.options.stage === 'prod' ? 'production' : 'development',
   entry: slsw.lib.entries,
   target: 'node',
   output: {
