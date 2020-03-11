73c79aac72708951a6412f6bad19c43dcc43b7f5
Marcus Bernal <marcus.bernal@slalom.com>
2020-03-11 14:15:14 -0600

    Initial implementation of DynamoDB in the domain repository.

--- src/repositories/domain.repository.js
+++ src/repositories/domain.repository.js
@@ -1,19 +1,43 @@
 /* Infrastructure specific, should not contain any business logic */
-// import * as AWS from 'aws-sdk';
+import AWS from 'aws-sdk';
+import env from '../config/env';
 import logger from '../services/logging.service';
 
-// const params = {};
-// const dynamo = new AWS.DynamoDB(params);
+const config = {
+  region: env.region,
+};
+const database = new AWS.DynamoDB.DocumentClient(config);
 
-export const getEntity = entity => {
-  logger.debug('docClient:', 'get entity', entity);
-  // return await dynamo.getItem(entity).promise();
-  return {
-    something: 'Unique',
+export const getEntity = id => {
+  logger.debug('docClient: get entity:', { id });
+
+  const params = {
+    TableName: process.env.DATA_TABLE,
+    KeyConditions: {
+      PK: {
+        AttributeValueList: [id],
+        ComparisonOperator: 'EQ',
+      },
+    },
   };
+  
+  const result = await database.query(params).promise();
+
+  return result.Items.reduce((acc, cur) => {
+    acc.PK = cur.PK;
+    acc[cur.SK] = cur;
+  }, {});
 };
 
 export const putEntity = entity => {
-  logger.debug('docClient:', 'putting entity', entity);
-  // return await dynamo.putItem(entity).promise();
+  logger.debug('docClient: put entity:', { entity });
+
+  const params = {
+    TableName: process.env.DATA_TABLE,
+    Item: entity,
+  };
+  
+  await database.query(params).promise();
+
+  return entity;
 };
