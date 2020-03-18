77ab5ebaa27b49caf1fedc45a536a41764c6d32a
Marcus Bernal <marcus.bernal@slalom.com>
2020-03-18 14:40:23 -0600

    Configured jest for unit and integration testing.

---
+++ bnd/jest/jest.config.int.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rootDir: '../../',
+  moduleFileExtensions: ['js'],
+  setupFilesAfterEnv: ['<rootDir>/bnd/jest/jest-extended.js'],
+  setupFiles: ['<rootDir>/bnd/jest/jest-setup.js'],
+  testMatch: ['**/*.int.spec.js'],
+  testPathIgnorePatterns: ['jest.config.int.js'],
+  reporters: ['default', 'jest-junit', 'jest-stare'],
+  testResultsProcessor: './node_modules/jest-stare',
+};
