ab800c15e5414903f598256e4af9861eef7d03d8
Marcus Bernal <marcus.bernal@slalom.com>
2020-03-23 10:50:10 -0600

    Add source mapping.

--- bnd/webpack/webpack.config.js
+++ bnd/webpack/webpack.config.js
@@ -3,13 +3,16 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  bail: true, // exit on any error
   mode: slsw.lib.options.stage === 'prod' ? 'production' : 'development',
   entry: slsw.lib.entries,
   target: 'node',
+  devtool: 'nosources-source-map',
   output: {
     libraryTarget: 'commonjs',
     path: path.resolve(__dirname, '.webpack'),
     filename: '[name].js',
+    sourceMapFilename: '[file].map',
   },
   externals: [nodeExternals()],
 };
